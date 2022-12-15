909d9068e6861d0a2fa559aa12199e27fb265149
geovanarochamp <geovana.rocha.martins@gmail.com>
2022-12-15 16:02:13 -0300

    add Status component on history

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from './styles'
+import { HistoryContainer, HistoryList, Status } from './styles'
 
 export function History() {
 	return (
@@ -21,31 +21,41 @@ export function History() {
 							<td>Tarefa</td>
 							<td>20min</td>
 							<td>Há cerca de 2 meses</td>
-							<td>Concluido</td>
+							<td>
+								<Status statusColor="green">Concluido</Status>
+							</td>
 						</tr>
 						<tr>
 							<td>Tarefa</td>
 							<td>20min</td>
 							<td>Há cerca de 2 meses</td>
-							<td>Concluido</td>
+							<td>
+								<Status statusColor="green">Concluido</Status>
+							</td>
 						</tr>
 						<tr>
 							<td>Tarefa</td>
 							<td>20min</td>
 							<td>Há cerca de 2 meses</td>
-							<td>Concluido</td>
+							<td>
+								<Status statusColor="green">Concluido</Status>
+							</td>
 						</tr>
 						<tr>
 							<td>Tarefa</td>
 							<td>20min</td>
 							<td>Há cerca de 2 meses</td>
-							<td>Concluido</td>
+							<td>
+								<Status statusColor="yellow">Em andamento</Status>
+							</td>
 						</tr>
 						<tr>
 							<td>Tarefa</td>
 							<td>20min</td>
 							<td>Há cerca de 2 meses</td>
-							<td>Concluido</td>
+							<td>
+								<Status statusColor="red">Interrompido</Status>
+							</td>
 						</tr>
 					</tbody>
 				</table>
--- src/pages/History/styles.ts
+++ src/pages/History/styles.ts
@@ -28,7 +28,7 @@ export const HistoryList = styled.div`
 			padding: 1rem;
 			text-align: left;
 			color: ${(props) => props.theme['gray-100']};
-			font-size: 0%.875rem;
+			font-size: 0.875rem;
 			line-height: 1.6;
 		}
 
@@ -58,5 +58,28 @@ export const HistoryList = styled.div`
 		td:last-child {
 			padding-right: 1.5rem;
 		}
+	}
+`
+const STATUS_COLORS = {
+	yellow: 'yellow-500',
+	green: 'green-500',
+	red: 'red-500',
+} as const
+
+interface StatusProps {
+	statusColor: keyof typeof STATUS_COLORS
+}
+
+export const Status = styled.span<StatusProps>`
+	display: flex;
+	align-items: center;
+	gap: 0.5rem;
+
+	&::before {
+		content: '';
+		width: 0.5rem;
+		height: 0.5rem;
+		border-radius: 9999px;
+		background: ${(props) => props.theme[STATUS_COLORS[props.statusColor]]};
 	}
 `
